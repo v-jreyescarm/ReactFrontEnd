b2db1f6ad887d63adf98893b2f0f085abaa49a10
Jeshua Reyes <jeshuakrc@gmail.com>
2022-03-26 21:25:57 -0600

    xd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola mundo xd
+          Pejibayes
         </p>
         <a
           className="App-link"
